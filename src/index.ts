63a60e58486ef96a328f8b73338a26bce4e683fa
yoonhero <yoonhero06@naver.com>
2022-05-03 20:39:01 +0900

    .

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
-import { init } from "./myPackage";
+import crypto from "crypto";
 
-init({ debug: true, url: "a" });
+interface BlockShape {
+    hash: string;
+    prevHash: string;
+    height: number;
+    data: string;
+}
+
+class Block implements BlockShape {
+    public hash: string;
+    constructor(
+        public prevHash: string,
+        public height: number,
+        public data: string
+    ) {
+        this.hash = Block.calculateHash(prevHash, height, data);
+    }
+
+    static calculateHash(prevHash: string, height: number, data: string) {
+        const toHash = `${prevHash}${height}${data}`;
+        return crypto.createHash("sha256").update(toHash).digest("hex");
+    }
+}
